70c41759401fdfb8efc2bde2d9800fabb0eb73e3
Maxel-Uds <maxellopes32@gmail.com>
2023-05-10 21:17:26 -0300

    Dart básico (variáveis, funções, print)

---
+++ Dart-Basic/functions_variables_and_print/android/app/src/main/kotlin/com/flutter/course/functions_variables_and_print/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.course.functions_variables_and_print
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Dart-Basic/functions_variables_and_print/lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  String nome = "Maxel";
+  int a = 1;
+  int b = 2;
+
+  int c = soma(a, b);
+
+  print("Soma: $c");
+  print("Nome: $nome");
+}
+
+int soma(int a, int b) {
+  return a + b;
+}
