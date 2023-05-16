a156c3cef26b9a0eabf3235201a50a2de4528fd5
Maxel-Uds <maxellopes32@gmail.com>
2023-05-15 21:05:51 -0300

    Listas em Dart

--- Dart-Basic/functions_variables_and_print/lib/main.dart
+++ Dart-Basic/functions_variables_and_print/lib/main.dart
@@ -8,6 +8,23 @@ void main() {
 
   print("Soma: $c");
   print("Nome: ${pessoa.nome}");
+
+  //Listas
+  var c1 = Carro("Celta");
+  var c2 = Carro("Corsa");
+  var c3 = Carro("Vectra");
+  var c4 = Carro("Kombi");
+
+  // List<Carro> carros = <Carro>[];
+  var carros = [];
+  carros.add(c1);
+  carros.add(c2);
+  carros.add(c3);
+  carros.add(c4);
+
+  for(Carro c in carros) {
+    print("Nome: ${c.nome}");
+  }
 }
 
 class Calculadora {
@@ -20,4 +37,10 @@ class Pessoa {
   String nome;
 
   Pessoa(this.nome);
+}
+
+class Carro {
+  String nome;
+
+  Carro(this.nome);
 }
