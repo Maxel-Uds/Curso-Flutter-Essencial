17721700271ddae27f15ba78901a8dfc466c79ab
Maxel-Uds <maxellopes32@gmail.com>
2023-05-17 16:16:52 -0300

    Text / TextStyle

--- basic_flutter_and_widgets/lib/main.dart
+++ basic_flutter_and_widgets/lib/main.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-
 class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primaryColor: Colors.blue),
-        home: const HomePage()
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const HomePage(),
     );
   }
 }
@@ -22,11 +23,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Flutter"),
-          centerTitle: true
+      appBar: AppBar(
+        title: const Text("Hello Flutter"),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: const Center(
+          child: Text(
+            "Hello Flutter",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue,
+            ),
+          ),
         ),
-        body: Container(color: Colors.yellow)
+      ),
     );
   }
 }
