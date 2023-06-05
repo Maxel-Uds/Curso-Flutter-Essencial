d99328fb338cabb029a49e59fcf1bbd1fb5dff53
Maxel-Uds <maxellopes32@gmail.com>
2023-06-05 20:55:45 -0300

    Navegação de Telas (parte 3 - organizando as funções)

--- basic_flutter_and_widgets/lib/pages/home_page.dart
+++ basic_flutter_and_widgets/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:basic_flutter_and_widgets/pages/hello_page1.dart';
+import 'package:basic_flutter_and_widgets/pages/hello_page2.dart';
+import 'package:basic_flutter_and_widgets/pages/hello_page3.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -52,22 +54,22 @@ class HomePage extends StatelessWidget {
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [_button("ListView", context), _button("Page 2", context), _button("Page 3", context)],
+          children: [_button("ListView", context, () => _onClickNavigator(context, const HelloPage1())), _button("Page 2", context, () => _onClickNavigator(context, const HelloPage2())), _button("Page 3", context, () => _onClickNavigator(context, const HelloPage3()))],
         ),
         Container(
           margin: const EdgeInsets.only(top: 20),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [_button("Snack", context), _button("Dialog", context), _button("Toast", context)],
+            children: [_button("Snack", context, _onClickSnack), _button("Dialog", context, _onClickDialog), _button("Toast", context, _onClickToast)],
           ),
         )
       ],
     );
   }
 
-  _button(String text, BuildContext context) {
+  _button(String text, BuildContext context, Function onClick) {
     return ElevatedButton(
-      onPressed: () => _onClick(context),
+      onPressed: () => onClick(),
       style: ButtonStyle(
         minimumSize: MaterialStateProperty.all(const Size(100, 50)),
         backgroundColor:
@@ -80,8 +82,8 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  _onClick(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => const HelloPage1()));
+  _onClickNavigator(BuildContext context, Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => page));
   }
 
   _img(String id) {
@@ -102,4 +104,10 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+  _onClickSnack() {}
+
+  _onClickDialog() {}
+
+  _onClickToast() {}
 }
