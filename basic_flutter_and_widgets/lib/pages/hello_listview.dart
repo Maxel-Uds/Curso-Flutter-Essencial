e652432e112f47421814eac449f746321657d59f
Maxel-Uds <maxellopes32@gmail.com>
2023-06-08 15:24:55 -0300

    ListView dinâmico - builder (parte 3)

--- basic_flutter_and_widgets/lib/pages/hello_listview.dart
+++ basic_flutter_and_widgets/lib/pages/hello_listview.dart
@@ -16,6 +16,14 @@ class HelloListView extends StatelessWidget {
   }
 
   _body(BuildContext context) {
+    List<Widget> imgs = [
+      _img("1"),
+      _img("2"),
+      _img("3"),
+      _img("4"),
+      _img("5"),
+    ];
+
     return ScrollConfiguration(
       behavior: ScrollConfiguration.of(context).copyWith(
         dragDevices: {
@@ -23,16 +31,12 @@ class HelloListView extends StatelessWidget {
           PointerDeviceKind.mouse,
         },
       ),
-      child: ListView(
-        itemExtent: 500,
-        children: [
-          _img("1"),
-          _img("2"),
-          _img("3"),
-          _img("4"),
-          _img("5"),
-        ],
-      ),
+      child: ListView.builder(
+          itemCount: imgs.length,
+          itemExtent: 300,
+          itemBuilder: (context, index) {
+          return imgs[index];
+      }),
     );
   }
 
