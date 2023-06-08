d1ec5d64b1b8adae7cf9061da7b2b7a5745ca351
Maxel-Uds <maxellopes32@gmail.com>
2023-06-08 19:04:18 -0300

    SafeArea

--- basic_flutter_and_widgets/lib/widgets/drawer_list.dart
+++ basic_flutter_and_widgets/lib/widgets/drawer_list.dart
@@ -5,36 +5,38 @@ class DrawerList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          const UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/78319246?s=96&v=4)"),
-              ),
-              accountName: Text("Maxel Udson"),
-              accountEmail: Text("maxellopes32@gmail.com")),
-          ListTile(
-            leading: const Icon(Icons.star),
-            title: const Text("Favoritos"),
-            subtitle: const Text("Cães favoritos"),
-            trailing: const Icon(Icons.start),
-            onTap: () {
-              print("Item 1");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.help),
-            title: const Text("Ajuda"),
-            subtitle: const Text("Suporte"),
-            trailing: const Icon(Icons.start),
-            onTap: () {
-              print("Ajuda");
-              Navigator.pop(context);
-            },
-          )
-        ],
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/78319246?s=96&v=4)"),
+                ),
+                accountName: Text("Maxel Udson"),
+                accountEmail: Text("maxellopes32@gmail.com")),
+            ListTile(
+              leading: const Icon(Icons.star),
+              title: const Text("Favoritos"),
+              subtitle: const Text("Cães favoritos"),
+              trailing: const Icon(Icons.start),
+              onTap: () {
+                print("Item 1");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text("Ajuda"),
+              subtitle: const Text("Suporte"),
+              trailing: const Icon(Icons.start),
+              onTap: () {
+                print("Ajuda");
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
