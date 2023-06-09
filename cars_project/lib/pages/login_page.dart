72bc37a66dcf19f2301e40c3d3d5910f7b714557
Maxel-Uds <maxellopes32@gmail.com>
2023-06-09 19:05:19 -0300

    Criando a estrutura básica do projeto

---
+++ cars_project/lib/pages/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Login"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container();
+  }
+}
