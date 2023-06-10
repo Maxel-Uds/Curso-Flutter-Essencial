a45771d29cd3d7694a8ed46512fd7181d2d4d7b6
Maxel-Uds <maxellopes32@gmail.com>
2023-06-10 13:27:41 -0300

    Dica - criando widgets com métodos

--- cars_project/lib/pages/login_page.dart
+++ cars_project/lib/pages/login_page.dart
@@ -19,37 +19,38 @@ class LoginPage extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: ListView(
         children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: "Login",
-              labelStyle: TextStyle(fontSize: 20, color: Colors.grey),
-              hintText: "Digite o login",
-              hintStyle: TextStyle(fontSize: 16, height: 2),
-            ),
-          ),
+          _textFormField("Login", "Digite o login"),
           const SizedBox(height: 10),
-          TextFormField(
-            obscureText: true,
-            decoration: const InputDecoration(
-              labelText: "Senha",
-              labelStyle: TextStyle(fontSize: 20, color: Colors.grey),
-              hintText: "Digite a senha",
-              hintStyle: TextStyle(fontSize: 16, height: 2),
-            ),
-          ),
+          _textFormField("Senha", "Digite a senha", isPassInput: true),
           const SizedBox(height: 20),
-          SizedBox(
-            height: 50,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text(
-                "Entrar",
-                style: TextStyle(fontSize: 22),
-              ),
-            ),
-          )
+          _button("Entrar")
         ],
       ),
     );
+  }
+
+  _button(String label) {
+    return SizedBox(
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              label,
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+        );
+  }
+
+  _textFormField(String label, String hint, { bool isPassInput = false }) {
+    return TextFormField(
+          obscureText: isPassInput,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: const TextStyle(fontSize: 20, color: Colors.grey),
+            hintText: hint,
+            hintStyle: const TextStyle(fontSize: 16, height: 2),
+          ),
+        );
   }
 }
