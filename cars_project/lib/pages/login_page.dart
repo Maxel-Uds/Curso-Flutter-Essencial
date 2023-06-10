eb6930d0902671df44432caa565d0e4023f59286
Maxel-Uds <maxellopes32@gmail.com>
2023-06-10 13:52:03 -0300

    TextEditingController (Lendo e alterando valores dos inputs gerados pelo TextFormField)

--- cars_project/lib/pages/login_page.dart
+++ cars_project/lib/pages/login_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final _controllerLogin = TextEditingController();
+  final _controllerPass = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,38 +22,50 @@ class LoginPage extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: ListView(
         children: [
-          _textFormField("Login", "Digite o login"),
+          _textFormField("Login", "Digite o login",
+              controller: _controllerLogin),
           const SizedBox(height: 10),
-          _textFormField("Senha", "Digite a senha", isPassInput: true),
+          _textFormField("Senha", "Digite a senha",
+              controller: _controllerPass, isPassInput: true),
           const SizedBox(height: 20),
-          _button("Entrar")
+          _button("Entrar", () => _onClickLogin())
         ],
       ),
     );
   }
 
-  _button(String label) {
+  _button(String label, Function onPressed) {
     return SizedBox(
-          height: 50,
-          child: ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              label,
-              style: const TextStyle(fontSize: 22),
-            ),
-          ),
-        );
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () => onPressed(),
+        child: Text(
+          label,
+          style: const TextStyle(fontSize: 22),
+        ),
+      ),
+    );
   }
 
-  _textFormField(String label, String hint, { bool isPassInput = false }) {
+  _textFormField(String label, String hint,
+      {bool isPassInput = false, required TextEditingController controller}) {
     return TextFormField(
-          obscureText: isPassInput,
-          decoration: InputDecoration(
-            labelText: label,
-            labelStyle: const TextStyle(fontSize: 20, color: Colors.grey),
-            hintText: hint,
-            hintStyle: const TextStyle(fontSize: 16, height: 2),
-          ),
-        );
+      controller: controller,
+      obscureText: isPassInput,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(fontSize: 20, color: Colors.grey),
+        hintText: hint,
+        hintStyle: const TextStyle(fontSize: 16, height: 2),
+      ),
+    );
+  }
+
+  _onClickLogin() {
+    String login = _controllerLogin.text;
+    String pass = _controllerPass.text;
+
+    print("Login $login");
+    print("Pass $pass");
   }
 }
