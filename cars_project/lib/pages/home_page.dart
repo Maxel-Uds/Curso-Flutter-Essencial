0acc0d3564457a932db2f9968a03b858020e5c9e
Maxel-Uds <maxellopes32@gmail.com>
2023-06-19 20:33:40 -0300

    TabBar - listener

--- cars_project/lib/pages/home_page.dart
+++ cars_project/lib/pages/home_page.dart
@@ -4,7 +4,6 @@ import 'package:cars_project/pages/pokemon_list_view_page.dart';
 import 'package:cars_project/widgets/drawer_list.dart';
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -13,41 +12,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage> {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.index = 2;
+    _tabController.addListener(() {
+      print(_tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // MyApp.of(context).changeTheme(Brightness.dark);
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Pokemons"),
-          actions: [_buildIconButtonList()],
-          bottom: const TabBar(tabs: [
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Pokemons"),
+        actions: [_buildIconButtonList()],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
             Tab(text: "A"),
             Tab(text: "B"),
             Tab(text: "C"),
             Tab(text: "D")
-          ],),
+          ],
         ),
-        body: const TabBarView(children: [
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
           PokemonListViewPage(Letter.A),
           PokemonListViewPage(Letter.B),
           PokemonListViewPage(Letter.C),
           PokemonListViewPage(Letter.D)
-        ]),
-        drawer: DrawerList("Maxel Udson", "maxellopes32@gmail.com"),
+        ],
       ),
+      drawer: DrawerList("Maxel Udson", "maxellopes32@gmail.com"),
     );
   }
 
   Row _buildIconButtonList() {
-    return MyApp.of(context).brightness.name.startsWith(Brightness.light.name) ?
-    _buildThemeModeButton(Icons.toggle_off, () => setState(() => MyApp.of(context).changeTheme(Brightness.dark)), Icons.dark_mode) :
-    _buildThemeModeButton(Icons.toggle_on, () => setState(() => MyApp.of(context).changeTheme(Brightness.light)), Icons.sunny);
+    return MyApp.of(context).brightness.name.startsWith(Brightness.light.name)
+        ? _buildThemeModeButton(
+            Icons.toggle_off,
+            () =>
+                setState(() => MyApp.of(context).changeTheme(Brightness.dark)),
+            Icons.dark_mode)
+        : _buildThemeModeButton(
+            Icons.toggle_on,
+            () =>
+                setState(() => MyApp.of(context).changeTheme(Brightness.light)),
+            Icons.sunny);
   }
 
-  Row _buildThemeModeButton(IconData toggle, Function switchTheme, IconData themeIcon) {
+  Row _buildThemeModeButton(
+      IconData toggle, Function switchTheme, IconData themeIcon) {
     return Row(
       children: [
         Icon(themeIcon),
@@ -62,4 +84,3 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     );
   }
 }
-
